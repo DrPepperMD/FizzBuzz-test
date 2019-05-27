e912e62650e8c846a0171f21460595dee5e2bed5
DrPepperMD <raymondjb8@gmail.com>
2019-05-27 22:04:44 +1000

    FizzBuzz Test 001

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("FizzBuzz!");
+            int one = 1;
+            bool test1 = true;
+            if (test1) 
+            {
+                int test1end = 3 / one;
+                Console.WriteLine(test1end);
+            }
+        }
+    }
+}
