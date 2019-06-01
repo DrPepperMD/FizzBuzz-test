ebedf5afefb66544b0a445f3c627d8cddf127ba7
DrPepperMD <raymondjb8@gmail.com>
2019-06-01 22:28:57 +1000

    FizzBuzz 004
    11 moar digits!!!@%!@

--- Program.cs
+++ Program.cs
@@ -11,8 +11,10 @@ namespace FizzBuzz
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
+
             Console.WriteLine("FizzBuzz!");
             Console.WriteLine("---------");
+
             int one = 1;
             bool onetest = true;
             if (onetest)
@@ -37,7 +39,7 @@ namespace FizzBuzz
             {
                 int threetest1 = three / 3;
                 threetest1 = 0;
-                Console.WriteLine("Fizz");
+                Console.WriteLine("Fizz (3)");
             }
 
             int four = 4;
@@ -55,21 +57,21 @@ namespace FizzBuzz
             {
                 int fivetest1 = five / 5;
                 fivetest1 = 0;
-                Console.WriteLine("buzz");
+                Console.WriteLine("Buzz (5)");
             }
 
             int six = 6;
             bool sixtest = true;
-            if (threetest)
+            if (sixtest)
             {
                 int sixtest1 = six / 3;
                 sixtest1 = 0;
-                Console.WriteLine("Fizz");
+                Console.WriteLine("Fizz (6)");
             }
 
             int seven = 7;
             bool seventest = true;
-            if (fourtest)
+            if (seventest)
             {
                 int seventest1 = seven / 3;
                 seventest1 = 0;
@@ -82,16 +84,117 @@ namespace FizzBuzz
             {
                 int eighttest1 = eight / 3;
                 eighttest1 = 0;
-                Console.WriteLine("buzz");
+                Console.WriteLine("8");
             }
 
             int nine = 9;
             bool ninetest = true;
-            if (thninetest)
+            if (ninetest)
             {
                 int ninetest1 = nine / 3;
                 ninetest1 = 0;
-                Console.WriteLine("Fizz");
+                Console.WriteLine("Fizz (9)");
+            }
+
+            int ten = 10;
+            bool tentest = true;
+            if (tentest)
+            {
+                int tentest1 = ten / 10;
+                tentest1 = 0;
+                Console.WriteLine("Buzz (9)");
+            }
+
+            // \/ double digits down here! \/
+
+            int eleven = 11;
+            bool eleventest = true;
+            if (eleventest)
+            {
+                int eleventest1 = eleven / 3;
+                eleventest1 = 0;
+                Console.WriteLine("11");
+            }
+
+            int twelve = 12;
+            bool twelvetest = true;
+            if (twelvetest)
+            {
+                int twelvetest1 = twelve / 3;
+                twelvetest1 = 0;
+                Console.WriteLine("Fizz (12)");
+            }
+
+            int thirteen = 13;
+            bool thirteentest = true;
+            if (thirteentest)
+            {
+                int thirteentest1 = thirteen / 3;
+                thirteentest1 = 0;
+                Console.WriteLine("13");
+            }
+
+            int four = 4;
+            bool fourtest = true;
+            if (fourtest)
+            {
+                int fourtest1 = four / 3;
+                fourtest1 = 0;
+                Console.WriteLine("4");
+            }
+
+            int five = 5;
+            bool fivetest = true;
+            if (fivetest)
+            {
+                int fivetest1 = five / 5;
+                fivetest1 = 0;
+                Console.WriteLine("Buzz (5)");
+            }
+
+            int six = 6;
+            bool sixtest = true;
+            if (sixtest)
+            {
+                int sixtest1 = six / 3;
+                sixtest1 = 0;
+                Console.WriteLine("Fizz (6)");
+            }
+
+            int seven = 7;
+            bool seventest = true;
+            if (seventest)
+            {
+                int seventest1 = seven / 3;
+                seventest1 = 0;
+                Console.WriteLine("7");
+            }
+
+            int eight = 8;
+            bool eighttest = true;
+            if (eighttest)
+            {
+                int eighttest1 = eight / 3;
+                eighttest1 = 0;
+                Console.WriteLine("8");
+            }
+
+            int nine = 9;
+            bool ninetest = true;
+            if (ninetest)
+            {
+                int ninetest1 = nine / 3;
+                ninetest1 = 0;
+                Console.WriteLine("Fizz (9)");
+            }
+
+            int ten = 10;
+            bool tentest = true;
+            if (tentest)
+            {
+                int tentest1 = ten / 10;
+                tentest1 = 0;
+                Console.WriteLine("Buzz (9)");
             }
 
         }
